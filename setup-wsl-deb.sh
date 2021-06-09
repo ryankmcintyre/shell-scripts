0042e800d7a1d656033a56284f9408b5beacbea4
Ryan McIntyre <ryankmcintyre@gmail.com>
2021-06-09 16:32:40 -0600

    Cleanup output and add dotnet install, cron job setup, and dev folders.

--- setup-wsl-deb.sh
+++ setup-wsl-deb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+echo "Installing tools"
+echo "----------"
 VIM_DIR=~/.vim/colors			# vim directory
 LIST_OF_APPS="git tmux curl vim jq gnupg-agent" 	# programs to install
 
@@ -8,81 +9,105 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 
 sudo apt-get install -y $LIST_OF_APPS 
+echo -e "done\n"
 
 ##########
 
 # Git prompt
-echo -n "Getting git-prompt.sh"
-echo -n ""
+echo "Getting git-prompt.sh"
+echo "----------"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
-echo "done"
+echo -e "done\n"
 
 # Get dircolors for Bash console
-echo -n "Getting and applying dircolors"
-echo -n ""
+echo "Getting and applying dircolors"
+echo "----------"
 curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark > ~/.dircolors
-echo "done"
+echo -e "done\n"
 
 # Add vim colors
-echo -n "Creating $VIM_DIR and getting gruvbox"
+echo "Creating $VIM_DIR and getting gruvbox"
+echo "----------"
 mkdir -p $VIM_DIR
-echo -n ""
 curl https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim > $VIM_DIR/gruvbox.vim
-echo "done"
+echo -e "done\n"
 
 #########
 
 # az cli
-echo -n "Installing Azure CLI"
-echo -n ""
+echo "Installing Azure CLI"
+echo "----------"
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y apt-transport-https azure-cli 
-echo -n ""
+echo ""
 
 # docker
-echo -n "Installing docker"
-echo -n ""
-echo -n "Adding Docker's GPG Key"
+echo "Installing docker"
+echo "----------"
+echo "Adding Docker's GPG Key"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo -n "Adding Docker stable repository"
+echo "Adding Docker stable repository"
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-echo -n "Installing Docker CE"
+echo "Installing Docker CE"
 sudo apt-get update
 if grep -q Microsoft /proc/version; then
     # This is WSL1 on Windows
-    echo -n "WSL1, installing docker-ce only"
+    echo "WSL1, installing docker-ce only"
     sudo apt-get install -y docker-ce
     sudo usermod -aG docker $USER
-    echo -n "Done with Docker, need to logout and back in for new docker permissions to be reflected"
+    echo -e "Done with Docker, need to logout and back in for new docker permissions to be reflected\n"
 elif grep -q microsoft /proc/version; then
     # This is WSL2 on Windows
-    echo -n "WSL2, skipping Docker install assuming Docker for Windows will be used."
+    echo -e "WSL2, skipping Docker install assuming Docker for Windows will be used.\n"
 else
     # Straight Linux and we can install full docker
-    echo -n "Straight Linux, installing full Docker"
+    echo "Straight Linux, installing full Docker"
     #sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     #sudo systemctl enable docker
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
-    echo -n "Starting docker service"
+    echo "Starting docker service"
     sudo service docker start
     sudo usermod -aG docker $USER
-    echo -n "Done with Docker, need to logout and back in for new docker permissions to be reflected"
+    echo -e "Done with Docker, need to logout and back in for new docker permissions to be reflected\n"
 fi
 
 # kubectl cli
-echo -n "Installing kubectl CLI"
-echo -n ""
+echo "Installing kubectl CLI"
+echo "----------"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-echo "done"
+echo -e "done\n"
+
+# dotnet
+echo "Installing dotnet"
+echo "----------"
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
+echo -e "done\n"
+
+# Create cron and add permission for user to start from .bashrc due to RAM issue https://github.com/microsoft/WSL/issues/4166#issuecomment-604707989
+echo "Setting up cron job for reclaiming RAM"
+echo "----------"
+echo '%sudo ALL=NOPASSWD: /etc/init.d/cron start' | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/cron
+echo -e "$(sudo crontab -u root -l 2>/dev/null)\n*/10 * * * * echo 3 > /proc/sys/vm/drop_caches; touch /root/drop_caches_last_run" | sudo crontab -u root -
+echo -e "done\n"
+
+# Create dev folders
+echo "Creating dev folders"
+echo "----------"
+mkdir ~/scratch
+mkdir ~/github
+echo -e "done\n"
